282292a98c3ead65c41928eb6916fb109c0544b1
rendykml <rendikamaluddin2018@gmail.com>
2025-08-02 22:32:33 +0700

    push

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
+// Deklarasi variabel a dan memberi nilai 7
+var a;
+a = 7;
+
+// Deklarasi variabel b dan menetapkan nilai a ke b
+var b;
+b = a;
+
+// Penjelasan:
+// Variabel a diberi nilai 7.
+// Nilai dari a (yaitu 7) kemudian diberikan ke variabel b.
+// Sekarang, b juga bernilai 7.
+
 var ces;
