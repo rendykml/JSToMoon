ac64e8aacf7b25b31a45feb373033236c6f67db8
rendykml <rendikamaluddin2018@gmail.com>
2025-08-02 23:03:15 +0700

    push

--- day2.js
+++ day2.js
@@ -5,4 +5,8 @@ let secondCharacter;
 secondCharacter = "Test";
 secondCharacter = character;
 
+let count = 8;
+let rows = ["Naomi", "Quincy", "CamperChan"];
+console.log(rows[0]);
+
 console.log(secondCharacter);
