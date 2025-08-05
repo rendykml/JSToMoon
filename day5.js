b42e1c09a80c0611073f36be028792e6c7ebd7c5
rendykml <rendikamaluddin2018@gmail.com>
2025-08-06 02:29:49 +0700

    push

--- day5.js
+++ day5.js
@@ -17,3 +17,11 @@ function padRow(name) {
 console.log(padRow());
 const ambilPadrow = padRow();
 console.log(ambilPadrow);
+
+function padRow(name) {
+  const test = "Testing";
+  console.log("This works!");
+  return test; //menyetopkan nilai test ke variabel yang memanggil fungsi ini
+  console.log("This works!");//funsi ini tidak akan pernah dieksekusi karena sudah ada return sebelumnya
+
+}
