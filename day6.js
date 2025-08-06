2547b778ed1458566288567ac2c7fd5e252f00e5
rendykml <rendikamaluddin2018@gmail.com>
2025-08-06 20:16:53 +0700

    push

--- day6.js
+++ day6.js
@@ -5,3 +5,7 @@ function padRow(rowNumber, rowCount) {
 for (let i = 0; i < count; i = i + 1) {
   rows.push(padRow(i + 1, count));
 }
+
+function padRow(rowNumber, rowCount) {
+  return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
+}
