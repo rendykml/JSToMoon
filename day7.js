94bc469c318d5ef80940c1dbd49fbc9bc12f229c
rendykml <rendikamaluddin2018@gmail.com>
2025-08-07 23:54:26 +0700

    commet

--- day7.js
+++ day7.js
@@ -9,3 +9,9 @@ for (let i = 0; i < count; i++) {
 for (let i = 1; i <= count; i++) {
   rows.push(padRow(i, count)); // This loop starts from 1 to count, inclusive
 }
+
+/*
+for (let i = 1; i <= count; i++) {
+  rows.push(padRow(i, count));
+}
+*/
