8dedfdeb856ce16c9d5d4c6957dcb3d41aaebdba
rendykml <rendikamaluddin2018@gmail.com>
2025-08-10 15:56:07 +0700

    puush

--- day9.js
+++ day9.js
@@ -3,9 +3,8 @@ while (rows.length < count) {
   rows.push(padRow(rows.length + 1, count));
 }
 
-for (let i = count; i > 0 ; i--){
-    rows.push(padRow(i, count));
-
+for (let i = count; i > 0; i--) {
+  rows.push(padRow(i, count));
 }
 
 const numbers = [1, 2, 3];
@@ -13,11 +12,13 @@ const shifted = numbers.shift();
 const unshifted = numbers.unshift(5);
 console.log(unshifted);
 console.log(numbers);
-console.log(shifted)
+console.log(shifted);
 
 let inverted = true;
 for (let i = 1; i <= count; i++) {
-if (inverted) {
-  rows.unshift(padRow(i, count));
-    }
+  if (inverted) {
+    rows.unshift(padRow(i, count));
+  } else {
+    rows.push(padRow(i, count));
+  }
 }
