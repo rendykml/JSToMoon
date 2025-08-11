3d00087557bf40cfe3b0b73d44374c03522e7b21
rendykml <rendikamaluddin2018@gmail.com>
2025-08-11 23:00:47 +0700

    push

--- day10.js
+++ day10.js
@@ -10,3 +10,19 @@ function getAverage(scores)  { // Function to calculate the average of an array 
 
 console.log(getAverage([92, 88, 12, 77, 57, 100, 67, 38, 97, 89]));
 console.log(getAverage([45, 87, 98, 100, 86, 94, 67, 88, 94, 95]))
+
+function getGrade(score) {
+  if (score === 100) {
+    return "A++";
+  } else if (score >= 90) {
+    return "A";
+  } else if (score >= 80) {
+    return "B";
+  } else if (score >= 70) {
+    return "C";
+  } else if (score >= 60) {
+    return "D";
+  } else {
+    return "F";
+  }
+}
