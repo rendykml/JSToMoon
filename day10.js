c40dbb890dfcc4feefe1d163b100a6cf40e00de0
rendykml <rendikamaluddin2018@gmail.com>
2025-08-11 22:53:15 +0700

    push

---
+++ day10.js
@@ -0,0 +1,12 @@
+function getAverage(scores) {
+  let sum = 0;
+  let pembagi = scores.length;
+  for(let i = 0; i < pembagi; i++){
+    sum = sum + scores[i];
+    
+  }
+  return sum/pembagi
+}
+
+console.log(getAverage([92, 88, 12, 77, 57, 100, 67, 38, 97, 89]));
+console.log(getAverage([45, 87, 98, 100, 86, 94, 67, 88, 94, 95]))
