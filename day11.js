c3d087d57720025fa4276c43d3d7b49d14ee460f
rendykml <rendikamaluddin2018@gmail.com>
2025-08-13 15:10:37 +0700

    push

--- day11.js
+++ day11.js
@@ -29,3 +29,4 @@ function studentMsg(totalScores, studentScore) {
 }
 console.log(studentMsg([92, 88, 12, 77, 57, 100, 67, 38, 97, 89], 37));
 console.log(studentMsg([56, 23, 89, 42, 75, 11, 68, 34, 91, 19], 100));
+
