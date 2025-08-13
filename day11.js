c354e218b92fb6d0061ee7f85442052e746089f1
rendykml <rendikamaluddin2018@gmail.com>
2025-08-13 14:58:04 +0700

    push

--- day11.js
+++ day11.js
@@ -13,3 +13,19 @@ function studentMsg(totalScores, studentScore) {
   return 
 }
 console.log(studentMsg([92, 88, 12, 77, 57, 100, 67, 38, 97, 89], 37));
+
+function studentMsg(totalScores, studentScore) {
+  let rataRataKelasBrok = getAverage(totalScores)
+  let grade = getGrade(studentScore);
+  let kataKataLulus= "";
+
+  if(studentScore >= rataRataKelasBrok){
+    kataKataLulus = " You passed the course."
+  } else{ 
+    kataKataLulus = " You failed the course."
+  }
+
+  return "Class average: " + rataRataKelasBrok + " Your grade: " + grade + "."+ kataKataLulus
+}
+console.log(studentMsg([92, 88, 12, 77, 57, 100, 67, 38, 97, 89], 37));
+console.log(studentMsg([56, 23, 89, 42, 75, 11, 68, 34, 91, 19], 100));
