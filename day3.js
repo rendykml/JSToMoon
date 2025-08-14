1aa65a3997b1c817f620c0a5c3b85827e1a7cb6f
rendykml <rendikamaluddin2018@gmail.com>
2025-08-14 23:57:21 +0700

    push

--- day3.js
+++ day3.js
@@ -1,8 +1,10 @@
 const count = 8;
 const rows = [];
 for (let i = 0; i < count; i = i + 1) {
+
  i;
-}
+} 
+// This code initializes an empty array called `rows` and fills it with numbers from 0 to `count - 1`.
 for (let i = 0; i < count; i = i + 1) {
   rows.push(i);
 }
