b0ee73f1be23fe7e4ccf2b65c084c97c3fa3195a
rendykml <rendikamaluddin2018@gmail.com>
2025-08-17 16:19:51 +0700

    push

--- day12js/script.js
+++ day12js/script.js
@@ -5,3 +5,5 @@ let currentWeaponIndex = 0;
 let fighting;
 
 const button1 = document.querySelector("#button1");
+const button2 = document.querySelector("#button2");
+const button3 = document.querySelector("#button3");
