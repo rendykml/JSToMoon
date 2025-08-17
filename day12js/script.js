f93ebc5d8df929fa32c67e2f194892009969b8ee
rendykml <rendikamaluddin2018@gmail.com>
2025-08-17 17:01:50 +0700

    push

--- day12js/script.js
+++ day12js/script.js
@@ -7,3 +7,11 @@ let fighting;
 const button1 = document.querySelector("#button1");
 const button2 = document.querySelector("#button2");
 const button3 = document.querySelector("#button3");
+
+const text = document.querySelector("#text");
+const xpText = document.querySelector("#xpText");
+const healthText = document.querySelector("#healthText");
+const goldText = document.querySelector("#goldText");
+const monsterStats = document.querySelector("#monsterStats");
+const monsterName = document.querySelector("#monsterName");
+const monsterHealthText = document.querySelector("#monsterHealth");
