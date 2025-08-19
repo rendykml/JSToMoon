d025fbfc93b9c0de00564717997645bae131233c
rendykml <rendikamaluddin2018@gmail.com>
2025-08-19 19:27:08 +0700

    ppush

--- day12js/script.js
+++ day12js/script.js
@@ -6,7 +6,7 @@ let fighting;
 let monsterHealth;
 let inventory = ["stick"];
 
-const button1 = document.querySelector('#button1');
+const button1 = document.querySelector("#button1");
 const button2 = document.querySelector("#button2");
 const button3 = document.querySelector("#button3");
 const text = document.querySelector("#text");
@@ -21,8 +21,18 @@ const locations = [
     name: "town square",
     "button text": ["Go to store", "Go to cave", "Fight dragon"],
     "button functions": [goStore, goCave, fightDragon],
-    text : "You are in the town square. You see a sign that says \"Store\".",
-  }
+    text: 'You are in the town square. You see a sign that says "Store".',
+  },
+  {
+    name: "store",
+    "button text": [
+      "Buy 10 health (10 gold)",
+      "Buy weapon (30 gold)",
+      "Go to town square",
+    ],
+    "button functions": [buyHealth, buyWeapon, goTown],
+    text: "You enter the store.",
+  },
 ];
 
 // initialize buttons
@@ -30,9 +40,7 @@ button1.onclick = goStore;
 button2.onclick = goCave;
 button3.onclick = fightDragon;
 
-function update(location) {
-
-}
+function update(location) {}
 
 function goTown() {
   button1.innerText = "Go to store";
@@ -41,7 +49,8 @@ function goTown() {
   button1.onclick = goStore;
   button2.onclick = goCave;
   button3.onclick = fightDragon;
-  text.innerText = "You are in the town square. You see a sign that says \"Store\".";
+  text.innerText =
+    'You are in the town square. You see a sign that says "Store".';
 }
 
 function goStore() {
@@ -62,10 +71,6 @@ function fightDragon() {
   console.log("Fighting dragon.");
 }
 
-function buyHealth() {
+function buyHealth() {}
 
-}
-
-function buyWeapon() {
-
-}
+function buyWeapon() {}
