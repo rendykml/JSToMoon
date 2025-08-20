772430e009d7af59944e439c9d7ab42d1747ddca
rendykml <rendikamaluddin2018@gmail.com>
2025-08-20 22:27:00 +0700

    push

--- day12js/script.js
+++ day12js/script.js
@@ -107,6 +107,7 @@ function buyWeapon() {
     gold -= 30;
     currentWeaponIndex++;
     goldText.innerText = gold;
+    let newWeapon = weapons[currentWeaponIndex];
     text.innerText = "You now have a new weapon.";
   }
 }
