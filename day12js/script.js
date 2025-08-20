256e10d64ee7360d0296e482050a509a9282e293
rendykml <rendikamaluddin2018@gmail.com>
2025-08-20 22:06:29 +0700

    push

--- day12js/script.js
+++ day12js/script.js
@@ -17,7 +17,24 @@ const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
 
-const weapons = []
+const weapons = [
+  {
+    name: "stick",
+    power: 5,
+  },
+  {
+    name: "dagger",
+    power: 30,
+  },
+  {
+    name: "claw hammer",
+    power: 50,
+  },
+  {
+    name: "sword",
+    power: 100,
+  },
+];
 
 const locations = [
   {
@@ -80,7 +97,7 @@ function buyHealth() {
     health += 10;
     goldText.innerText = gold;
     healthText.innerText = health;
-  }else {
+  } else {
     text.innerText = "You do not have enough gold to buy health.";
   }
 }
