321500c26da82d092c962e2afc5893e161882052
rendykml <rendikamaluddin2018@gmail.com>
2025-08-21 14:05:09 +0700

    push

--- day12js/script.js
+++ day12js/script.js
@@ -57,6 +57,12 @@ const locations = [
     "button text": ["Fight slime", "Fight fanged beast", "Go to town square"],
     "button functions": [fightSlime, fightBeast, goTown],
     text: "You enter the cave. You see some monsters."
+  }, {
+    name: "fight",
+    "button text": ["Attack", "Dodge", "Run"],
+    "button functions": [attack, dodge, goTown],
+    text: "You are fighting a monster."
+
   }
 ];
 
@@ -132,18 +138,27 @@ function sellWeapon() {
 
 function fightSlime() {
   fighting = 0;
-  monster[0]
-  goFight()
+  goFight();
 }
 
 function fightBeast() {
-
+  fighting = 1;
+  goFight();
 }
 
 function fightDragon() {
-  console.log("Fighting dragon.");
+  fighting = 2;
+  goFight();
 }
 
 function goFight() {
+
+}
+
+function attack() {
+
+}
+
+function dodge() {
 
 }
