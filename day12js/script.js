73b566a7a1030d615b9b3fb39cb43fa578546c07
rendykml <rendikamaluddin2018@gmail.com>
2025-08-21 16:08:23 +0700

    pus

--- day12js/script.js
+++ day12js/script.js
@@ -75,6 +75,12 @@ const locations = [
     "button text": ["REPLAY?", "REPLAY?", "REPLAY?"],
     "button functions": [restart, restart, restart],
     text: "You die. &#x2620;"
+  },
+  { 
+    name: "win", 
+    "button text": ["REPLAY?", "REPLAY?", "REPLAY?"], 
+    "button functions": [restart, restart, restart], 
+    text: "You defeat the dragon! YOU WIN THE GAME! &#x1F389;" 
   }
 ];
 
@@ -175,7 +181,7 @@ function goFight() {
 function attack() {
   text.innerText = "The " + monsters[fighting].name + " attacks.";
   text.innerText += " You attack it with your " + weapons[currentWeaponIndex].name + ".";
-  health -= monsters[fighting].level;
+  health -= getMonsterAttackValue(monsters[fighting].level);
   monsterHealth -= weapons[currentWeaponIndex].power + Math.floor(Math.random() * xp) + 1;
   healthText.innerText = health;
   monsterHealthText.innerText = monsterHealth;
@@ -188,6 +194,10 @@ function attack() {
       defeatMonster();
     }
   }
+}
+
+function getMonsterAttackValue(level){
+  
 }
 
 function dodge() {
@@ -206,7 +216,7 @@ function lose() {
   update(locations[5]);
 }
 
-function winGame(){
+function winGame() {
   update(locations[6]);
 }
 
