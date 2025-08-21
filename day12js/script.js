a762e2e901d74a75a6e8494b7d33433e01f9f4c6
rendykml <rendikamaluddin2018@gmail.com>
2025-08-21 13:41:31 +0700

    push

--- day12js/script.js
+++ day12js/script.js
@@ -110,7 +110,10 @@ function sellWeapon() {
     gold += 15;
     goldText.innerText = gold;
     let currentWeapon = inventory.shift();
-
+    text.innerText = "You sold a " + currentWeapon + ".";
+    text.innerText += " In your inventory you have: " + inventory;
+  } else {
+    text.innerText = "Don't sell your only weapon"
   }
 }
 
