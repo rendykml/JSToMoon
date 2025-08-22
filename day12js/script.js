00b22c9cdde1f34957e7344b4a71613c1cecf9e2
rendykml <rendikamaluddin2018@gmail.com>
2025-08-22 23:27:52 +0700

    push

--- day12js/script.js
+++ day12js/script.js
@@ -194,7 +194,7 @@ function attack() {
     } else {
       defeatMonster();
     }
-    if (Math.random() <= .1) {
+    if (Math.random() <= .1 && inventory.length !== 1) {
       text.innerText += " Your " + inventory.pop() + " breaks.";
       currentWeaponIndex--;
   }
