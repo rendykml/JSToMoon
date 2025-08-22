03e00feda0d6742068962cf76b37219744cfa404
rendykml <rendikamaluddin2018@gmail.com>
2025-08-22 23:24:55 +0700

    push

--- day12js/script.js
+++ day12js/script.js
@@ -195,7 +195,7 @@ function attack() {
       defeatMonster();
     }
     if (Math.random() <= .1) {
-      text.innerText += " Yours " + inventory.pop() + " breaks."
+      text.innerText += " Your " + inventory.pop() + " breaks."
   }
   }
    if(isMonsterHit()){
