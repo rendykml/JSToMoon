9da6156636d51fb956e7239788620b6c19d0114c
rendykml <rendikamaluddin2018@gmail.com>
2025-08-25 21:14:22 +0700

    push

--- day12js/script.js
+++ day12js/script.js
@@ -267,6 +267,7 @@ function pickEight() {
 function pick(guess) {
   const numbers = [];
   while (numbers.length < 10) {
-    numbers.push(Math.floor(Math.random() * 11))
+    numbers.push(Math.floor(Math.random() * 11));
   }
+  text.innerText = "You picked " + guess + ". Here are the random numbers:\n";
 }
