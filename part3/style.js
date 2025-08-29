8dabcf5e09b7bf55e99730d99be48e4373bf434a
rendykml <rendikamaluddin2018@gmail.com>
2025-08-29 17:34:16 +0700

    push

--- part3/style.js
+++ part3/style.js
@@ -25,4 +25,5 @@ function changeBackgroundColor() {
   bgHexCodeSpanElement.innerText = color;
   body.style.backgroundColor = color;
 }
-changeBackgroundColor();
+const btn = document.querySelector("#btn");
+console.log(btn);
