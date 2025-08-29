2247c5c518d4154dd4c9fcdf96086f54458c7aea
rendykml <rendikamaluddin2018@gmail.com>
2025-08-29 17:30:56 +0700

    push

--- part3/style.js
+++ part3/style.js
@@ -11,9 +11,12 @@ const darkColorsArr = [
   "#800020",
 ];
 function getRandomIndex() {
-  console.log(Math.floor(darkColorsArr.length * Math.random()));
   const randomIndex = Math.floor(darkColorsArr.length * Math.random());
   return randomIndex;
 }
 
 const body = document.querySelector("body");
+const bgHexCodeSpanElement = document.querySelector("#bg-hex-code");
+
+
+console.log(bgHexCodeSpanElement);
