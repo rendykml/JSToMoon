58d5638f1b6a0890a510e543ad29fef1aefa5451
rendykml <rendikamaluddin2018@gmail.com>
2025-08-31 20:37:31 +0700

    push

--- part4_form/script.js
+++ part4_form/script.js
@@ -17,5 +17,6 @@ function isInvalidInput(str) {
 }
 
 function addEntry() {
-
+  const targetId = '#' + entryDropdown.value;
+  const targetInputContainer = document.querySelector(`${targetId} .input-container`);
 }
