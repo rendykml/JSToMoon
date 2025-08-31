87edeb47b103739bfbbadb789b15f47ca66d2223
rendykml <rendikamaluddin2018@gmail.com>
2025-08-31 18:35:47 +0700

    push

--- part4_form/script.js
+++ part4_form/script.js
@@ -8,7 +8,9 @@ let isError = false;
 
 function cleanInputString(str) {
   const regex = /[+-\s]/g;
-  
-  return str.replace(regex,"");
+  return str.replace(regex, '');
+}
 
+function isInvalidInput(str) {
+  const regex = /e/i;
 }
