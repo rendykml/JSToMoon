52e09f22df1b2a2c1481f4ae99e8ab01b0b8ab65
rendykml <rendikamaluddin2018@gmail.com>
2025-09-01 22:08:35 +0700

    push

--- part4_form/script.js
+++ part4_form/script.js
@@ -18,7 +18,7 @@ function isInvalidInput(str) {
 
 function addEntry() {
   const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`);
-  const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length;
+  const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length + 1;
   const HTMLString = `
   <label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
   <input type="text" id="${entryDropdown.value}-${entryNumber}-name" placeholder="Name" />
@@ -32,4 +32,4 @@ function addEntry() {
   targetInputContainer.innerHTML += HTMLString;
 }
 
-addEntryButton.addEventListener("click",addEntry);
+addEntryButton.addEventListener("click", addEntry);
