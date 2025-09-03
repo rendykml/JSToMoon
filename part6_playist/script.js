b2c8fb0f50d5e250bd534939e59dd5033c102f0f
rendykml <rendikamaluddin2018@gmail.com>
2025-09-03 14:08:34 +0700

    push

--- part6_playist/script.js
+++ part6_playist/script.js
@@ -90,3 +90,12 @@ const printGreeting = () => {
 }
 
 printGreeting();
+
+const printMessage = org => {
+  console.log(`${org} is awesome!`);
+}
+
+printMessage('freeCodeCamp');
+const addTwoNumbers = (num1, num2) => num1 + num2;
+
+console.log(addTwoNumbers(3,4));
