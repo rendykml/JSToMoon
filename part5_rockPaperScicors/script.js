2271e52af160862083bb31a4b091754d49c1d5ff
rendykml <rendikamaluddin2018@gmail.com>
2025-09-03 10:34:36 +0700

    push

--- part5_rockPaperScicors/script.js
+++ part5_rockPaperScicors/script.js
@@ -1,9 +1,32 @@
 function getRandomComputerResult() {
   const options = ["Rock", "Paper", "Scissors"];
-  const random = Math.floor(Math.random()*options.length)
-  ;
-
-  return options[random];
-
+  const randomIndex = Math.floor(Math.random() * options.length);
+  return options[randomIndex];
 }
-console.log(getRandomComputerResult());
+
+function hasPlayerWonTheRound(player, computer) {
+  let playerMenang = true;
+
+  if(player==="Rock" && computer==="Scissors"){
+    playerMenang = true
+  }else if(player ==="Scissors" && computer==="Rock"){
+    playerMenang = false;
+  }else if(player === "Paper" && computer==="Rock"){
+    playerMenang = true;
+  }else if(player===computer){
+    playerMenang = false;
+  }else if(player==="Scissors" && computer==="Rock"){
+    playerMenang = false;
+  } else if(player==="Rock" && computer==="Paper"){
+    playerMenang = false;
+  }else if(player==="Scissors" && computer==="Paper"){
+    playerMenang = true;
+  } 
+  else {
+    playerMenang = false;
+  }
+  return playerMenang;
+}
+
+console.log(hasPlayerWonTheRound("Rock", "Scissors")); 
+console.log(hasPlayerWonTheRound("Scissors", "Rock")); 
