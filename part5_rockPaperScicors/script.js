51153cf53a4adebe0b509c6e8d29cbae399c5289
rendykml <rendikamaluddin2018@gmail.com>
2025-09-03 11:34:38 +0700

    pus

--- part5_rockPaperScicors/script.js
+++ part5_rockPaperScicors/script.js
@@ -4,9 +4,6 @@ function getRandomComputerResult() {
   return options[randomIndex];
 }
 
-let playerScore = 0;
-let computerScore = 0;
-
 function hasPlayerWonTheRound(player, computer) {
   return (
     (player === "Rock" && computer === "Scissors") ||
@@ -14,23 +11,60 @@ function hasPlayerWonTheRound(player, computer) {
     (player === "Paper" && computer === "Rock")
   );
 }
+
+let playerScore = 0;
+let computerScore = 0;
+
 function getRoundResults(userOption) {
   const computerResult = getRandomComputerResult();
-  const check = hasPlayerWonTheRound(userOption, computerResult);
-  let message = ""
-  if(check === true){
-    message = "Player wins! " + userOption + " beats " + computerResult; 
-    playerScore += 1
-  } else if(userOption === computerResult){
-    message = `It's a tie! Both chose ${userOption}`
-  } else {
-    message = `Computer wins! ${computerResult} beats ${userOption}`
-    computerScore += 1
-  }
 
-  return message;
- 
+  if (hasPlayerWonTheRound(userOption, computerResult)) {
+    playerScore++;
+    return `Player wins! ${userOption} beats ${computerResult}`;
+  } else if (computerResult === userOption) {
+    return `It's a tie! Both chose ${userOption}`;
+  } else {
+    computerScore++;
+    return `Computer wins! ${computerResult} beats ${userOption}`;
+  }
 }
 
-console.log(getRoundResults("Rock"));
-console.log("Player Score: ", playerScore, "Computer Score: ", computerScore);
+const playerScoreSpanElement = document.getElementById("player-score");
+const computerScoreSpanElement = document.getElementById("computer-score");
+const roundResultsMsg = document.getElementById("results-msg");
+const winnerMsgElement = document.getElementById("winner-msg");
+const optionsContainer = document.querySelector(".options-container");
+const resetGameBtn = document.getElementById("reset-game-btn");
+
+function showResults(userOption) {
+  roundResultsMsg.innerText = getRoundResults(userOption);
+  computerScoreSpanElement.innerText = computerScore;
+  playerScoreSpanElement.innerText = playerScore;
+
+  if(playerScore === 3){
+    winnerMsgElement.innerText = "Player has won the game!"; 
+    resetGameBtn.style.display = "block";
+    optionsContainer.style.display = "none";
+  } else if(computerScore===3){
+    winnerMsgElement.innerText = "Computer has won the game!";
+    resetGameBtn.style.display = "block";
+    optionsContainer.style.display = "none"
+  }
+
+};
+
+const rockBtn = document.getElementById("rock-btn");
+const paperBtn = document.getElementById("paper-btn");
+const scissorsBtn = document.getElementById("scissors-btn");
+
+rockBtn.addEventListener("click", function () {
+  showResults("Rock");
+});
+
+paperBtn.addEventListener("click", function () {
+  showResults("Paper");
+});
+
+scissorsBtn.addEventListener("click", function () {
+  showResults("Scissors");
+});
