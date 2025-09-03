04dbc5064b91d720ec14ae1cb0c8b2a687b2aee6
rendykml <rendikamaluddin2018@gmail.com>
2025-09-03 14:11:56 +0700

    push

--- part6_playist/script.js
+++ part6_playist/script.js
@@ -79,23 +79,13 @@ const allSongs = [
 ];
 
 const audio = new Audio();
+
 let userData = {
   songs: [...allSongs],
   currentSong: null,
   songCurrentTime: 0,
 };
 
-const printGreeting = () => {
-  console.log('Hello there!'); 
-}
-
-printGreeting();
-
-const printMessage = org => {
-  console.log(`${org} is awesome!`);
-}
-
-printMessage('freeCodeCamp');
-const addTwoNumbers = (num1, num2) => num1 + num2;
-
-console.log(addTwoNumbers(3,4));
+const renderSongs = (array) => {
+const songsHTML = array.map()
+};
