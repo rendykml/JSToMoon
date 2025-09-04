8c4af05666b86ce59014cb841f0daa66db6bd48c
rendykml <rendikamaluddin2018@gmail.com>
2025-09-04 10:59:16 +0700

    push

--- part6_playist/script.js
+++ part6_playist/script.js
@@ -92,9 +92,13 @@ const playSong = (id) => {
 
   if (userData?.currentSong === null || userData?.currentSong.id !== song.id) {
     audio.currentTime = 0;
+  } else {
+    audio.currentTime = userData?.songCurrentTime;
   }
 
-
+  userData.currentSong = song;
+  playButton.classList.add("playing")
+  audio.play()
 };
 
 const renderSongs = (array) => {
