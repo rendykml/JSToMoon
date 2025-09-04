e9249aa1c703dce93deea94a8780a7f146048695
rendykml <rendikamaluddin2018@gmail.com>
2025-09-04 14:01:11 +0700

    push

--- part6_playist/script.js
+++ part6_playist/script.js
@@ -111,6 +111,8 @@ const pauseSong = () => {
 const playNextSong = () => {
   if (userData?.currentSong === null) {
     playSong(userData?.songs[0].id);
+  } else {
+    const currentSongIndex = getCurrentSongIndex()
   }
 
 }
