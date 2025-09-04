a9fe07d2baa8f8bdab6eedc64c70f0cc3855c59e
rendykml <rendikamaluddin2018@gmail.com>
2025-09-04 11:14:19 +0700

    push

--- part6_playist/script.js
+++ part6_playist/script.js
@@ -101,12 +101,16 @@ const playSong = (id) => {
   audio.play();
 };
 
+const pauseSong = () => {
+  userData.songCurrentTime = audio.currentTime
+};
+
 const renderSongs = (array) => {
   const songsHTML = array
     .map((song)=> {
       return `
       <li id="song-${song.id}" class="playlist-song">
-      <button onclick="playSong(${song.id})" class="playlist-song-info">
+      <button class="playlist-song-info" onclick="playSong(${song.id})">
           <span class="playlist-song-title">${song.title}</span>
           <span class="playlist-song-artist">${song.artist}</span>
           <span class="playlist-song-duration">${song.duration}</span>
@@ -124,9 +128,9 @@ const renderSongs = (array) => {
 };
 
 playButton.addEventListener("click", () => {
-  if (userData?.currentSong === null) {
+    if (userData?.currentSong === null) {
     playSong(userData?.songs[0].id);
-  }else {
+  } else {
     playSong(userData?.currentSong.id);
   }
 });
@@ -147,5 +151,4 @@ const sortSongs = () => {
   return userData?.songs;
 };
 
-
-renderSongs(userData?.songs);
+renderSongs(sortSongs());
