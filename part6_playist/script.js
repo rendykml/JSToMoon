31372ad47cbbe7ae7b668d76288f831960611982
rendykml <rendikamaluddin2018@gmail.com>
2025-09-05 14:12:48 +0700

    push

--- part6_playist/script.js
+++ part6_playist/script.js
@@ -144,7 +144,7 @@ const shuffle = () => {
 };
 
 const deleteSong = (id) => {
-    if (userData?.currentSong?.id === id) {
+  if (userData?.currentSong?.id === id) {
     userData.currentSong = null;
     userData.songCurrentTime = 0;
 
@@ -202,6 +202,25 @@ const renderSongs = (array) => {
     .join("");
 
   playlistSongs.innerHTML = songsHTML;
+
+  if (userData?.songs.length === 0) {
+    const resetButton = document.createElement("button");
+    const resetText = document.createTextNode("Reset Playlist");
+
+    resetButton.id = "reset";
+    resetButton.ariaLabel = "Reset playlist";
+    resetButton.appendChild(resetText);
+    playlistSongs.appendChild(resetButton);
+
+    resetButton.addEventListener("click", () => {
+      userData.songs = [...allSongs];
+      renderSongs(sortSongs())
+      setPlayButtonAccessibleText();
+      resetButton.remove()
+    });
+
+  }
+
 };
 
 const setPlayButtonAccessibleText = () => {
