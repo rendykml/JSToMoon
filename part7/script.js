06f0194331ed12eb2329695b05d3e772a7ac0b29
rendykml <rendikamaluddin2018@gmail.com>
2025-09-06 10:04:30 +0700

    push

--- part7/script.js
+++ part7/script.js
@@ -10,12 +10,33 @@ function cleanInputString(str) {
 }
 
 const chekPalindrome = (str) => {
+   
     const reversedStr = cleanInputString(str.split('').reverse().join(''));
+     if (str === '') return '';
     return str === reversedStr;
 }
 
+const outputResult = (str)=> {
+    const hasilInput = input.value;
+    const clearInput = cleanInputString(hasilInput);
+    if(chekPalindrome(clearInput)){
+        result.style.color = 'green';
+        return result.textContent = `${hasilInput} is a palindrome`
+    } else if (chekPalindrome(cleanInputString(input.value)) === ''){
+        result.style.color = 'red';
+        return result.textContent = 'Please input a value'
+    } else if(!chekPalindrome(clearInput)){
+        result.style.color = 'orange';
+        return result.textConntent = `${hasilInput} is not a palindrome`
+    } else {
+        return result.textContent = 'Error'
+    }
+}
+
 chekBtn.addEventListener('click', () => {
+  
     const inputValue = input.value;
-    textInput.textContent = inputValue;
+    console.log(chekPalindrome(inputValue));
+    textInput.textContent = outputResult(inputValue);
 });
 
