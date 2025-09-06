9eae6e153b24f0dcd5dc416476db57196f1c1f0d
rendykml <rendikamaluddin2018@gmail.com>
2025-09-06 09:34:37 +0700

    pus

---
+++ part7/script.js
@@ -0,0 +1,21 @@
+const input = document.getElementById('inputText');
+const chekBtn = document.getElementById('check-btn');
+const result = document.getElementById('result');
+const textInput = document.getElementById('text-input');
+
+
+function cleanInputString(str) {
+  const regex = /[+-\s]/g;
+  return str.replace(regex, '');
+}
+
+const chekPalindrome = (str) => {
+    const reversedStr = cleanInputString(str.split('').reverse().join(''));
+    return str === reversedStr;
+}
+
+chekBtn.addEventListener('click', () => {
+    const inputValue = input.value;
+    textInput.textContent = inputValue;
+});
+
