b08cd0832ac92c90995d6cb03a8d7efea3ff66af
rendykml <rendikamaluddin2018@gmail.com>
2025-09-08 17:08:40 +0700

    push

--- part10/script.js
+++ part10/script.js
@@ -13,10 +13,18 @@ const descriptionInput = document.getElementById("description-input");
 const taskData = [];
 let currentTask = {};
 
-openTaskFormBtn.addEventListener("click", ()=>{
+openTaskFormBtn.addEventListener("click", () =>
   taskForm.classList.toggle("hidden")
-})
+);
 
-closeTaskFormBtn.addEventListener("click", ()=>{
-  taskForm.classList.toggle("hidden")
-})
+closeTaskFormBtn.addEventListener("click", () => {
+  confirmCloseDialog.showModal();
+});
+
+cancelBtn.addEventListener("click", () => confirmCloseDialog.close());
+
+discardBtn.addEventListener("click", () => {
+  confirmCloseDialog.close();
+  taskForm.classList.toggle("hidden");
+});
+
