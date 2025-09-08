70ed2318157fe71c22adea8fb0774af446b67558
rendykml <rendikamaluddin2018@gmail.com>
2025-09-08 14:43:08 +0700

    push

--- part9/script.js
+++ part9/script.js
@@ -199,13 +199,19 @@ const setPlayerCards = (arr = players) => {
 playersDropdownList.addEventListener("change", (e) => {
   playerCards.innerHTML = "";
 
-
   switch (e.target.value) {
     case "nickname":
       setPlayerCards(players.filter((player) => player.nickname !== null));
-      break
+      break;
+    case "forward":
+      setPlayerCards(players.filter((player) => player.position === "forward"));
+      break;
+
+  case "midfielder":
+    setPlayerCards(players.filter(
+      (player) => player.position === "midfielder"
+    ))
+    break
 
   }
-
-
 });
