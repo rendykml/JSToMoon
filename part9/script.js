a0f430f7ca1d6e2933b6995679f499c33c469eaf
rendykml <rendikamaluddin2018@gmail.com>
2025-09-08 13:50:26 +0700

    push

--- part9/script.js
+++ part9/script.js
@@ -180,6 +180,12 @@ teamName.textContent = team;
 worldCupYear.textContent = year;
 headCoach.textContent = coachName;
 
+const setPlayerCards = (arr = players) => {
 
-const setPlayerCards = (arr = players) => {};
 
+  playerCards.innerHTML += arr.map(({name, position, number, isCaptain, nickname}) => {
+    
+  })
+
+
+};
