17fcdac1707479553e452047fe13f04a9245457a
rendykml <rendikamaluddin2018@gmail.com>
2025-09-08 14:06:34 +0700

    push

--- part9/script.js
+++ part9/script.js
@@ -187,7 +187,10 @@ const setPlayerCards = (arr = players) => {
     ({ name, position, number, isCaptain, nickname }) => {
      return `
         <div class="player-card">
-          <h2>${isCaptain?"(Captain)": ""} ${name}</h2>
+          <h2>${isCaptain ? "(Captain)" : ""} ${name}</h2>
+          <p>Position: ${position}</p>
+          <p>Number: ${number}</p>
+          <p>Nickname: ${nickname? nickname: "N/A"}</p>
         </div>
       `;
     }
