7b44b51768233087002a7d3290873ffbe7ee8c2f
rendykml <rendikamaluddin2018@gmail.com>
2025-09-09 12:33:06 +0700

    push

--- part10/script.js
+++ part10/script.js
@@ -24,6 +24,8 @@ const addOrUpdateTask = () => {
 
   if (dataArrIndex === -1) {
     taskData.unshift(taskObj);
+  } else {
+    taskData[dataArrIndex] = taskObj;
   }
 
   updateTaskContainer()
@@ -68,8 +70,10 @@ const editTask = (buttonEl) => {
   titleInput.value = currentTask.title;
   dateInput.value = currentTask.date;
   descriptionInput.value = currentTask.description;
-  addOrUpdateTaskBtn.innerText = "Update Task"
-  
+
+  addOrUpdateTaskBtn.innerText = "Update Task";
+
+  taskForm.classList.toggle("hidden");  
 }
 
 const reset = () => {
@@ -86,6 +90,7 @@ openTaskFormBtn.addEventListener("click", () =>
 
 closeTaskFormBtn.addEventListener("click", () => {
   const formInputsContainValues = titleInput.value || dateInput.value || descriptionInput.value;
+
   if (formInputsContainValues) {
     confirmCloseDialog.showModal();
   } else {
