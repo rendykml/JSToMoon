f1eb911aaf6369d106dad3762d5a678382fec5ae
rendykml <rendikamaluddin2018@gmail.com>
2025-09-09 11:02:19 +0700

    push

--- part10/script.js
+++ part10/script.js
@@ -34,7 +34,13 @@ taskForm.addEventListener("submit", (e) => {
 
   const dataArrIndex = taskData.findIndex((item) => item.id === currentTask.id);
   const taskObj = {
-    id: `${titleInput.value.toLowerCase().split(' ').join('-')}-${Date.now()}`,  
+    id: `${titleInput.value.toLowerCase().split(" ").join("-")}-${Date.now()}`,
+    title: titleInput.value,
+    date: dateInput.value,
+    description: descriptionInput.value,
   };
-  console.log(taskObj);
+
+  if(dataArrIndex === -1){
+    taskData.unshift(taskObj)
+  }  
 });
