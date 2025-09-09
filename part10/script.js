01fbf80baf4a4f7da8bce027167a8642baecc3ff
rendykml <rendikamaluddin2018@gmail.com>
2025-09-09 10:37:47 +0700

    push

--- part10/script.js
+++ part10/script.js
@@ -28,3 +28,13 @@ discardBtn.addEventListener("click", () => {
   taskForm.classList.toggle("hidden");
 });
 
+taskForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const dataArrIndex = taskData.findIndex((item) => item.id === currentTask.id);
+
+  const taskObj = {
+    id: titleInput.value.toLowerCase().split(" ")
+  };
+  console.log(taskObj);
+});
