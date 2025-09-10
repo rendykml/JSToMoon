9c4363da56cbc63f2acd29be47cc64ebaf535891
rendykml <rendikamaluddin2018@gmail.com>
2025-09-10 19:47:18 +0700

    push

--- part11/script.js
+++ part11/script.js
@@ -1,9 +1,27 @@
+const a = () => {
+  return "freeCodeCamp " + b();
+};
+
+const b = () => {
+  return "is " + c();
+};
+
+const c = () => {
+  return "awesome!";
+};
+
+console.log(a())
+
 const numberInput = document.getElementById("number-input");
 const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
 
 const decimalToBinary = (input) => {
   let binary = "";
+
+  if (input === 0) {
+    binary = "0";
+  }
 
   while (input > 0) {
     binary = (input % 2) + binary;
