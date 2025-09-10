9635451e0fe34a24c33733f6a979a66905d1394e
rendykml <rendikamaluddin2018@gmail.com>
2025-09-10 10:31:04 +0700

    push

--- part7/script.js
+++ part7/script.js
@@ -1,53 +1,128 @@
-const input = document.getElementById('text-input');
-const chekBtn = document.getElementById('check-btn');
-const result = document.getElementById('result');
-// const textInput = document.getElementById('text-input');
-const reset = document.getElementById('resetButton');
+const taskForm = document.getElementById("task-form");
+const confirmCloseDialog = document.getElementById("confirm-close-dialog");
+const openTaskFormBtn = document.getElementById("open-task-form-btn");
+const closeTaskFormBtn = document.getElementById("close-task-form-btn");
+const addOrUpdateTaskBtn = document.getElementById("add-or-update-task-btn");
+const cancelBtn = document.getElementById("cancel-btn");
+const discardBtn = document.getElementById("discard-btn");
+const tasksContainer = document.getElementById("tasks-container");
+const titleInput = document.getElementById("title-input");
+const dateInput = document.getElementById("date-input");
+const descriptionInput = document.getElementById("description-input");
 
+const taskData = JSON.parse(localStorage.getItem("data")) || [];
+let currentTask = {};
 
-const cleanInputString = (str) => {
-    const regex = /[_\/.:,)(+-\s]/g;
- return str.toLowerCase().replace(regex, "");
+const removeSpecialChars = (val) => {
+  return val.trim().replace(/[^A-Za-z0-9\-\s]/g, '')
 }
 
+const addOrUpdateTask = () => {
+   if(!titleInput.value.trim()){
+    alert("Please provide a title");
+    return;
+  }
+  const dataArrIndex = taskData.findIndex((item) => item.id === currentTask.id);
+  const taskObj = {
+    id: `${removeSpecialChars(titleInput.value).toLowerCase().split(" ").join("-")}-${Date.now()}`,
+    title: removeSpecialChars(titleInput.value),
+    date: dateInput.value,
+    description: removeSpecialChars(descriptionInput.value),
+  };
 
-const chekPalindrome = (str) => {
-   
-    const reversedStr = cleanInputString(str.split('').reverse().join(''));
-     if (str === '') return '';
-    return str === reversedStr;
-}
+  if (dataArrIndex === -1) {
+    taskData.unshift(taskObj);
+  } else {
+    taskData[dataArrIndex] = taskObj;
+  }
 
-const clearInput = () => {
-    input.value = '';
-}
+  localStorage.setItem("data", JSON.stringify(taskData));
+  updateTaskContainer()
+  reset()
+};
 
-const outputResult = (str)=> {
-    const hasilInput = input.value;
-    const clearInput = cleanInputString(hasilInput);
-    if(chekPalindrome(clearInput)){
-        result.style.color = 'green';
-        return result.textContent = `${hasilInput} is a palindrome`
-    } else if (chekPalindrome(cleanInputString(input.value)) === ''){
-        result.style.color = 'red';
-        console.log(alert('Please input a value'))
-        return result.textContent = 'Please input a value'
-    } else if(!chekPalindrome(clearInput)){
-        result.style.color = 'orange';
-        return result.textConntent = `${hasilInput} is not a palindrome`
-    } else {
-        return result.textContent = 'Error'
+const updateTaskContainer = () => {
+  tasksContainer.innerHTML = "";
+
+  taskData.forEach(
+    ({ id, title, date, description }) => {
+        (tasksContainer.innerHTML += `
+        <div class="task" id="${id}">
+          <p><strong>Title:</strong> ${title}</p>
+          <p><strong>Date:</strong> ${date}</p>
+          <p><strong>Description:</strong> ${description}</p>
+          <button onclick="editTask(this)" type="button" class="btn">Edit</button>
+          <button onclick="deleteTask(this)" type="button" class="btn">Delete</button> 
+        </div>
+      `)
     }
+  );
+};
+
+
+const deleteTask = (buttonEl) => {
+  const dataArrIndex = taskData.findIndex(
+    (item) => item.id === buttonEl.parentElement.id
+  );
+
+  buttonEl.parentElement.remove();
+  taskData.splice(dataArrIndex, 1);
+  localStorage.setItem("data", JSON.stringify(taskData));
 }
 
-reset.addEventListener('click', clearInput);
+const editTask = (buttonEl) => {
+    const dataArrIndex = taskData.findIndex(
+    (item) => item.id === buttonEl.parentElement.id
+  );
 
-chekBtn.addEventListener('click', () => {
-    const inputValue = input.value;
-    console.log(chekPalindrome(inputValue));
-    result.textContent = outputResult(inputValue)
-    console.log(input.value);
-    console.log(cleanInputString(input.value))
+  currentTask = taskData[dataArrIndex];
+
+  titleInput.value = currentTask.title;
+  dateInput.value = currentTask.date;
+  descriptionInput.value = currentTask.description;
+
+  addOrUpdateTaskBtn.innerText = "Update Task";
+
+  taskForm.classList.toggle("hidden");  
+}
+
+const reset = () => {
+  addOrUpdateTaskBtn.innerText = "Add Task";
+  titleInput.value = "";
+  dateInput.value = "";
+  descriptionInput.value = "";
+  taskForm.classList.toggle("hidden");
+  currentTask = {};
+}
+
+if (taskData.length) {
+  updateTaskContainer();
+}
+
+openTaskFormBtn.addEventListener("click", () =>
+  taskForm.classList.toggle("hidden")
+);
+
+closeTaskFormBtn.addEventListener("click", () => {
+  const formInputsContainValues = titleInput.value || dateInput.value || descriptionInput.value;
+  const formInputValuesUpdated = titleInput.value !== currentTask.title || dateInput.value !== currentTask.date || descriptionInput.value !== currentTask.description;
+
+  if (formInputsContainValues && formInputValuesUpdated) {
+    confirmCloseDialog.showModal();
+  } else {
+    reset();
+  }
 });
 
+cancelBtn.addEventListener("click", () => confirmCloseDialog.close());
 
+discardBtn.addEventListener("click", () => {
+  confirmCloseDialog.close();
+  reset()
+});
+
+taskForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  addOrUpdateTask();
+});
