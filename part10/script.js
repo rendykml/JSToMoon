f10f62498288818515c51bd122ea7ac01715e19b
rendykml <rendikamaluddin2018@gmail.com>
2025-09-10 09:27:50 +0700

    push

--- part10/script.js
+++ part10/script.js
@@ -94,8 +94,7 @@ closeTaskFormBtn.addEventListener("click", () => {
 
   if (formInputsContainValues && formInputValuesUpdated) {
     confirmCloseDialog.showModal();
-  } 
-  else {
+  } else {
     reset();
   }
 });
@@ -112,3 +111,11 @@ taskForm.addEventListener("submit", (e) => {
 
   addOrUpdateTask();
 });
+
+const myTaskArr = [
+  { task: "Walk the Dog", date: "22-04-2022" },
+  { task: "Read some books", date: "02-11-2023" },
+  { task: "Watch football", date: "10-08-2021" },
+];
+
+localStorage.setItem("data", JSON.stringify(myTaskArr));
