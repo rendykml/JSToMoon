3bd4f9953b82095085337889139777aa8c48c01b
rendykml <rendikamaluddin2018@gmail.com>
2025-09-10 19:40:56 +0700

    push

--- part11/script.js
+++ part11/script.js
@@ -6,8 +6,8 @@ const decimalToBinary = (input) => {
   let binary = "";
 
   while (input > 0) {
-   input = Math.floor(input /2)
- 
+    binary = (input % 2) + binary;
+    input = Math.floor(input / 2);
   }
 
   result.innerText = binary;
