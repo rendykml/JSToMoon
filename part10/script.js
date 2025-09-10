3eb63866787b4e74e25ea1f76b11b02b584ccf42
rendykml <rendikamaluddin2018@gmail.com>
2025-09-10 09:42:48 +0700

    push

--- part10/script.js
+++ part10/script.js
@@ -119,3 +119,12 @@ const myTaskArr = [
 ];
 
 localStorage.setItem("data", JSON.stringify(myTaskArr));
+
+localStorage.clear("data", myTaskArr)
+localStorage.removeItem("data")
+
+const getTaskArr = localStorage.getItem("data")
+console.log(getTaskArr)
+
+const getTaskArrObj = JSON.parse(localStorage.getItem("data"));
+console.log(getTaskArrObj);
