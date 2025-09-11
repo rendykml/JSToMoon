c2c235642915b84a0dc95027cbc58445e47dd44a
rendykml <rendikamaluddin2018@gmail.com>
2025-09-11 14:47:27 +0700

    push

--- part11/script.js
+++ part11/script.js
@@ -46,8 +46,18 @@ const showAnimation = () => {
       `;
     }, obj.addElDelay);
 
-  setTimeout(()=>{},obj.showMsgDelay)
+    setTimeout(() => {
+      document.getElementById(obj.inputVal).textContent = obj.msg;
+    }, obj.showMsgDelay);
+
+    setTimeout(() => {
+      document.getElementById(obj.inputVal).remove();
+    }, obj.removeElDelay);
   });
+
+  setTimeout(() => {
+ result.textContent = decimalToBinary(5)
+  }, 20000);
 };
 
 const checkUserInput = () => {
