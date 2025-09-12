965ba3a7bdb837fb699e100dfdfffeb3f244ba1a
rendykml <rendikamaluddin2018@gmail.com>
2025-09-12 13:38:53 +0700

    push

--- part12/script.js
+++ part12/script.js
@@ -1,54 +1,52 @@
-const inputNumber = document.getElementById('number');
-const convertBtn = document.getElementById('convert-btn')
-const result = document.getElementById('output')
+const inputNumber = document.getElementById("number");
+const convertBtn = document.getElementById("convert-btn");
+const result = document.getElementById("output");
 
-
-const resultNumber = (input)=>{
-
-
-  if(input === 1000){
-    return + "M"
-  }else if(input >= 40){
-    return resultNumber(input - 40) + "LX"
-  } else if (input >= 10){
-    return resultNumber(input - 10)  + "X"
-  } else if (input === 9){
-    return resultNumber(input - 9) + "IX"
-  }else if (input >= 5){
-    return resultNumber(input - 5) + "V"
-  } else if(input === 4){
-    return resultNumber(input-4)+ "IV"
-  } else if(input >= 1){
-    return resultNumber(input-1 )+ "I"
-  } else if (input === 0){
-    return ""
-  } else{
-    return input
+const resultNumber = (input) => {
+  if (input === 1000) {
+    return "M";
+  }else if(input >= 400){
+    return "CD" + resultNumber(input - 400)
+    }else if (input >=100){
+    return "C" + resultNumber(input - 100)
+    }else if(input >=90){
+    return "XC" + resultNumber(input-90)
+  }else if(input >= 50 ){
+    return "L" + resultNumber(input - 50)
   }
-}
+  else if(input >= 40){
+    return "XL" + resultNumber(input-40)
+  } 
+  else if (input >= 10) {
+    return "X" + resultNumber(input - 10);
+  } else if (input === 9) {
+    return "IX";
+  } else if (input >= 5) {
+    return "V" + resultNumber(input - 5);
+  } else if (input === 4) {
+    return "IV";
+  } else if (input >= 1) {
+    return "I" + resultNumber(input - 1);
+  } else {
+    return "";
+  }
+};
 
+const checkResultNumber = () => {
+  const inputInt = parseInt(inputNumber.value);
 
-const checkResultNumber = ()=>{
-
-  const inputInt = parseInt(inputNumber.value) 
-
-  if(!inputNumber.value ||  isNaN(inputInt)){
-    result.textContent = "Please enter a valid number"
-  } else if(inputInt >= 4000){
-    result.textContent = "Please enter a number less than or equal to 3999"
-  } else if (inputInt < 0){
-    result.textContent = "Please enter a number greater than or equal to 1"
-  } else{
-    result.textContent = resultNumber(inputInt).split("").reverse().join("")
+  if (!inputNumber.value || isNaN(inputInt)) {
+    result.textContent = "Please enter a valid number";
+  } else if (inputInt >= 4000) {
+    result.textContent = "Please enter a number less than or equal to 3999";
+  } else if (inputInt < 0) {
+    result.textContent = "Please enter a number greater than or equal to 1";
+  } else {
+    result.textContent = resultNumber(inputInt).split("").reverse().join("");
     numberInput.value = "";
   }
+};
 
-
-
-}
-
-
-convertBtn.addEventListener("click", ()=>{
-  checkResultNumber()
-}
-)
+convertBtn.addEventListener("click", () => {
+  checkResultNumber();
+});
