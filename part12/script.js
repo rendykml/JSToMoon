6efc9312b7c95b20fb67cc2e3cfbb6bfc0f6c98d
rendykml <rendikamaluddin2018@gmail.com>
2025-09-12 12:15:09 +0700

    push

--- part12/script.js
+++ part12/script.js
@@ -4,8 +4,15 @@ const result = document.getElementById('output')
 
 
 const resultNumber = (input)=>{
-  if (input === 9){
-    return "IX"
+
+  if(input === 0 || input ===1){
+    return "selesai"
+  }else if (resultNumber(input) >= 9){
+    return retultNumber((input/9) + "IX")
+  } else if(input === 4){
+    return "IV"
+  } else if (input === 40){
+    return "XL"
   }
   return input
 }
@@ -16,7 +23,7 @@ const checkResultNumber = ()=>{
   const inputInt = parseInt(inputNumber.value) 
 
   if(!inputNumber.value ||  isNaN(inputInt)){
-    result.textContent = "Please enter a number greater than or equal to 1"
+    result.textContent = "Please enter a valid number"
   } else if(inputInt >= 4000){
     result.textContent = "Please enter a number less than or equal to 3999"
   } else if (inputInt <= 0){
@@ -26,8 +33,9 @@ const checkResultNumber = ()=>{
   }
 
 
+
 }
 
-convertBtn.addEventListener("click", ()=>{
-  console.log(20)
-})
+
+convertBtn.addEventListener("click", checkResultNumber
+)
