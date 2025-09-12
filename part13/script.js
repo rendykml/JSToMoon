2de1ee6d979ca875c003addcef3384c98363fe9f
rendykml <rendikamaluddin2018@gmail.com>
2025-09-12 16:40:25 +0700

    push

--- part13/script.js
+++ part13/script.js
@@ -2,9 +2,11 @@ const messageInput = document.getElementById("message-input");
 const result = document.getElementById("result");
 const checkMessageButton = document.getElementById("check-message-btn");
 
-const helpRegex = /please help/i;
+const helpRegex = /please help|assist me/i;
 
-const isSpam = (msg) => helpRegex.test(msg);
+const denyList = [helpRegex];
+
+const isSpam = (msg) => denyList.some((regex)=>regex.test(msg));
 
 checkMessageButton.addEventListener("click", () => {
   if (messageInput.value === "") {
