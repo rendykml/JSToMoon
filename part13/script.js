aff84111d9197e1de376dd21601f1303d6e5132d
rendykml <rendikamaluddin2018@gmail.com>
2025-09-12 16:19:57 +0700

    push

--- part13/script.js
+++ part13/script.js
@@ -2,7 +2,7 @@ const messageInput = document.getElementById("message-input");
 const result = document.getElementById("result");
 const checkMessageButton = document.getElementById("check-message-btn");
 
-const helpRegex = /please help/;
+const helpRegex = /please helps/;
 
 const isSpam = (msg) => false;
 
