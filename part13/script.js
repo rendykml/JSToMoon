6f6e924a4356a2091c4541d02a67cb8c1525017c
rendykml <rendikamaluddin2018@gmail.com>
2025-09-12 15:06:47 +0700

    pus

--- part13/script.js
+++ part13/script.js
@@ -1,4 +1,11 @@
-const messageInput = document.getElementById("message-input")
-const result = document.getElementById("result")
-const checkMessageButton = document.getElementById("check-message-btn")
+const messageInput = document.getElementById("message-input");
+const result = document.getElementById("result");
+const checkMessageButton = document.getElementById("check-message-btn");
 
+checkMessageButton.addEventListener("click", () => {
+  if(messageInput.value === ""){
+    alert("Please enter a message.");
+    return;
+  } 
+  
+});
