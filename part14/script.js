f2ee3a6b65195583eacb0e49439961b94d8fcd58
rendykml <rendikamaluddin2018@gmail.com>
2025-09-14 21:46:22 +0700

    push

--- part14/script.js
+++ part14/script.js
@@ -1,5 +1,7 @@
 const sortButton = document.getElementById("sort");
 
 const sortInputArray = (event) => {
-  event.preventDefault()
+  event.preventDefault();
 }
+
+sortButton.addEventListener("click", sortInputArray)
