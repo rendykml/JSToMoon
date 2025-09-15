6056427a9fb5e18732670d53817693d5fb4ec06f
rendykml <rendikamaluddin2018@gmail.com>
2025-09-15 08:43:17 +0700

    push

--- part14/script.js
+++ part14/script.js
@@ -2,8 +2,10 @@ const sortButton = document.getElementById("sort");
 
 const sortInputArray = (event) => {
   event.preventDefault();
-
-  const inputValues = [...document.getElementsByClassName("values-dropdown")];
+  const inputValues = [...document.getElementsByClassName("values-dropdown")].map((dropdown) => Number(dropdown.value));
+  // const inputValues = [...document.getElementsByClassName("values-dropdown")].map((dropdown) => dropdown.value);
+  console.log(inputValues);
 }
+
 
 sortButton.addEventListener("click", sortInputArray);
