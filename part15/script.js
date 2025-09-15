1962776f4be501c4f8dd332c85273d20bb583a8b
rendykml <rendikamaluddin2018@gmail.com>
2025-09-15 16:26:45 +0700

    push

--- part15/script.js
+++ part15/script.js
@@ -1,17 +1,12 @@
 const getMean = (array) => array.reduce((acc, el) => acc + el, 0) / array.length;
 
-const testArr1 = [1, 2, 3, 4, 5];
-const testArr2 = [1, 2, 3, 4, 5, 6];
-const isEven = testArr2.length % 2 === 0;
-console.log(isEven);
-const oddListMedian = testArr1[Math.floor(testArr1.length / 2)];
-console.log(oddListMedian);
-
-const evenListMedian = getMean([testArr2[testArr2.length / 2], testArr2[testArr2.length / 2] - 1]) 
-console.log(evenListMedian)
- 
 const getMedian = (array) => {
-  const sorted = array.sort((a, b) => a - b);
+  const sorted = array.toSorted((a, b) => a - b);
+  if(sorted.length % 2 === 0){
+    return getMean([sorted[sorted.length/2], sorted[sorted.length /2] -1])
+  } else if(sorted.length % 2 === 1){
+    return sorted[Math.floor(sorted.length/2)]
+  }
 }
 
 const calculate = () => {
