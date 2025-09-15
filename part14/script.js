b4aa89385901ad595d00e6f04d6d2072941eb22b
rendykml <rendikamaluddin2018@gmail.com>
2025-09-15 09:44:26 +0700

    pushh

--- part14/script.js
+++ part14/script.js
@@ -35,10 +35,21 @@ const bubbleSort = (array) => {
 
 const selectionSort = (array) => {
   for (let i = 0; i < array.length; i++) {
-    let minIndex = i
-    for(let j = i+1; j < array.length; j++){
+    let minIndex = i;
 
+    for (let j = i + 1; j < array.length; j++) {
+      console.log(array, array[j], array[minIndex]);
+      if (array[j] < array[minIndex]) {
+        minIndex = j;
+        
+      }
+      
+        
     }
+    const temp = array[i];
+array[i] = array[minIndex];
+        array[minIndex] = temp
+
   }
 }
 
