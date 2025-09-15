a494c362e1a803200f12fb12e62e71541e78a2b3
rendykml <rendikamaluddin2018@gmail.com>
2025-09-15 10:08:06 +0700

    push

--- part14/script.js
+++ part14/script.js
@@ -53,8 +53,14 @@ const selectionSort = (array) => {
 
 const insertionSort = (array) => {
   for (let i = 1; i < array.length; i++) {
-      const currValue = array[i];
-      let j = i-1
+    const currValue = array[i];
+    let j = i - 1;
+    
+    while (j >= 0 && array[j] > currValue) {
+      array[j + 1] = array[j];
+      j--;
+    }
+    array[j+1] = currValue
   }
 }
 
