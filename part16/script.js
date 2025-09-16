05228c76427d4d31d017fb539d8a749533de43f5
rendykml <rendikamaluddin2018@gmail.com>
2025-09-16 15:27:12 +0700

    push

--- part16/script.js
+++ part16/script.js
@@ -1,9 +1,6 @@
 const range = (start, end) => Array(end - start + 1).fill(start).map((element, index) => element + index);
 const charRange = (start, end) => range(start.charCodeAt(0), end.charCodeAt(0)).map(code => String.fromCharCode(code));
 
-console.log(range(1,9))
-console.log(charRange("A","Z"))
-
 window.onload = () => {
   const container = document.getElementById("container");
   const createLabel = (name) => {
@@ -14,5 +11,13 @@ window.onload = () => {
   }
   const letters = charRange("A", "J");
   letters.forEach(createLabel);
-  range(1,99).forEach((number)=>{})
+  range(1, 99).forEach(number => {
+     createLabel(number);
+    letters.forEach(letter => {
+     const input = document.createElement("input");
+     input.type = "text";
+     input.id = letter + number;
+     input.ariaLabel = letter + number
+    })   
+  })
 }
