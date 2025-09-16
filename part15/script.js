6e8b0a7476817b95545c9bf7a298df324d8aea9f
rendykml <rendikamaluddin2018@gmail.com>
2025-09-16 11:56:50 +0700

    push

--- part15/script.js
+++ part15/script.js
@@ -32,8 +32,12 @@ const getRange = (array) => {
 
 const getVariance = (array) => {
   const mean = getMean(array);
-
-  const variance = array.reduce((acc,el) =>{},0)
+  const variance = array.reduce((acc, el) => {
+    const difference = el - mean;
+    const squared = difference ** 2;
+    return acc + squared;
+  }, 0) / array.length;
+  return variance;
 }
 
 const calculate = () => {
@@ -45,9 +49,12 @@ const calculate = () => {
   const median = getMedian(numbers);
   const mode = getMode(numbers);
   const range = getRange(numbers);
+  const variance = getVariance(numbers);
 
+  document.querySelector("#variance").textContent = variance;
   document.querySelector("#mean").textContent = mean;
   document.querySelector("#median").textContent = median;
   document.querySelector("#mode").textContent = mode;
   document.querySelector("#range").textContent = range;
+
 }
