a368429f229856bb4877e6884e37e2bafc633e0d
rendykml <rendikamaluddin2018@gmail.com>
2025-09-16 15:36:00 +0700

    push

--- part16/script.js
+++ part16/script.js
@@ -1,3 +1,6 @@
+const isEven = (num) => num % 2 === 0
+const sum = nums => nums.reduce((acc, el) => acc + el, 0);
+
 const range = (start, end) => Array(end - start + 1).fill(start).map((element, index) => element + index);
 const charRange = (start, end) => range(start.charCodeAt(0), end.charCodeAt(0)).map(code => String.fromCharCode(code));
 
@@ -12,12 +15,13 @@ window.onload = () => {
   const letters = charRange("A", "J");
   letters.forEach(createLabel);
   range(1, 99).forEach(number => {
-     createLabel(number);
+    createLabel(number);
     letters.forEach(letter => {
-     const input = document.createElement("input");
-     input.type = "text";
-     input.id = letter + number;
-     input.ariaLabel = letter + number
-    })   
+      const input = document.createElement("input");
+      input.type = "text";
+      input.id = letter + number;
+      input.ariaLabel = letter + number;
+      container.appendChild(input);
+    })
   })
 }
