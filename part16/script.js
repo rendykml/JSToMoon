e4616c36016c03451a74f57024377ace7b0a365d
rendykml <rendikamaluddin2018@gmail.com>
2025-09-20 09:27:51 +0700

    push

--- part16/script.js
+++ part16/script.js
@@ -20,8 +20,9 @@ const spreadsheetFunctions = {
 const range = (start, end) => Array(end - start + 1).fill(start).map((element, index) => element + index);
 const charRange = (start, end) => range(start.charCodeAt(0), end.charCodeAt(0)).map(code => String.fromCharCode(code));
 
-const evalFormula = (x,cells) => {
-  
+const evalFormula = (x, cells) => {
+  const idToText = id => cells.find(cell => cell.id === id).value;
+  const rangeRegex = /([A-J])/
 }
 
 window.onload = () => {
