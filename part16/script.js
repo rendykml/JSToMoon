b2ed5e7c6ab6990b824b52cc4d48055d18d48314
rendykml <rendikamaluddin2018@gmail.com>
2025-09-22 16:40:54 +0700

    push

--- part16/script.js
+++ part16/script.js
@@ -55,7 +55,7 @@ const evalFormula = (x, cells) => {
   const cellRegex = /[A-J][1-9][0-9]?/gi;
   const cellExpanded = rangeExpanded.replace(cellRegex, match => idToText(match.toUpperCase()));
   const functionExpanded = applyFunction(cellExpanded);
-  return functionExpanded === x ? functionExpanded : evalFormula(functionExpanded, cells) 
+  return functionExpanded === x ? functionExpanded : evalFormula(functionExpanded, cells);
 }
 
 window.onload = () => {
@@ -85,6 +85,6 @@ const update = event => {
   const element = event.target;
   const value = element.value.replace(/\s/g, "");
   if (!value.includes(element.id) && value.startsWith('=')) {
-
+    element.value = evalFormula();
   }
 }
