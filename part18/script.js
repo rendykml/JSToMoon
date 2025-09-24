a96f6f87508426c89b03d9e459e587952b0c299c
rendykml <rendikamaluddin2018@gmail.com>
2025-09-24 17:32:31 +0700

    push

--- part18/script.js
+++ part18/script.js
@@ -115,7 +115,12 @@ class ShoppingCart {
 
     const totalCountPerProduct = {};
     this.items.forEach((dessert) => {
-      totalCountPerProduct[dessert.id] = (totalCountPerProduct[dessert.id] || 0) +1
+      totalCountPerProduct[dessert.id] = (totalCountPerProduct[dessert.id] || 0) + 1;
     })
+
+    const currentProductCount = totalCountPerProduct[product.id];
+    const currentProductCountSpan = document.getElementById(`product-count-for-id${id}`);
+
+    currentProductCount > 1 ? undefined : undefined;
   }
 };
