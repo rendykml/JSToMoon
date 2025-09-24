9eb459336d909e84ab9e488ca0979e1e05a679ca
rendykml <rendikamaluddin2018@gmail.com>
2025-09-24 15:36:39 +0700

    push

--- part18/script.js
+++ part18/script.js
@@ -86,5 +86,13 @@ const products = [
 ];
 
 products.forEach(
-  ({name, id, price, category}) => {}
+  ({ name, id, price, category }) => {
+    dessertCards.innerHTML += `
+      <div class="dessert-card">
+        <h2>${name}</h2>
+        <p class="dessert-price">$${price}</p>
+        <p class="product-category">Category: ${category}</p>
+      </div>
+    `;
+  }
 );
