d66ec63b2818b9e4641da223c742cfa6ed96990c
rendykml <rendikamaluddin2018@gmail.com>
2025-09-29 14:09:28 +0700

    push

--- part19/script.js
+++ part19/script.js
@@ -92,7 +92,13 @@ const movePlayer = (key, xVelocity, isPressed) => {
 
 
   switch (key) {
-    case "ArrowLeft" :
+    case "ArrowLeft":
+      keys.leftKey.pressed = isPressed;
+      if (xVelocity === 0) {
+        player.velocity.x = xVelocity;
+      }
+      player.velocity.x -= xVelocity
+      break
   }
 
 }
