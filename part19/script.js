defca5498b68ce8adf34b4adf18c0dc9bc6351f4
rendykml <rendikamaluddin2018@gmail.com>
2025-09-29 09:34:22 +0700

    push

--- part19/script.js
+++ part19/script.js
@@ -3,5 +3,8 @@ const canvas = document.getElementById("canvas");
 const startScreen = document.querySelector(".start-screen");
 const checkpointScreen = document.querySelector(".checkpoint-screen");
 const checkpointMessage = document.querySelector(".checkpoint-screen > p");
+const ctx = canvas.getContext("2d");
 
+
+canvas.width = innerWidth
 
