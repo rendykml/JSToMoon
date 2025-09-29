91fc8a8240d3899e48616393c2c64dbc7c57cfde
rendykml <rendikamaluddin2018@gmail.com>
2025-09-29 09:40:11 +0700

    pus

--- part19/script.js
+++ part19/script.js
@@ -4,7 +4,8 @@ const startScreen = document.querySelector(".start-screen");
 const checkpointScreen = document.querySelector(".checkpoint-screen");
 const checkpointMessage = document.querySelector(".checkpoint-screen > p");
 const ctx = canvas.getContext("2d");
+canvas.width = innerWidth;
+canvas.height = innerHeight;
+const gravity = 0.5;
 
-
-canvas.width = innerWidth
-
+let isCheckpointCollisionDetectionActive = true
