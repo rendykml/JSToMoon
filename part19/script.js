635554f3e8c33eb6d94e5212206f8cabe2bc5fad
rendykml <rendikamaluddin2018@gmail.com>
2025-09-29 11:00:52 +0700

    push

--- part19/script.js
+++ part19/script.js
@@ -58,7 +58,12 @@ class Player {
 
 const player = new Player();
 
-const animate = ()=>{}
+
+const animate = () => {
+  requestAnimationFrame(animate);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  player.update()
+}
 
 
 const startGame = () => {
