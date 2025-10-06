1eb8ed07e1accd56d18c31becc9eb00478f6f763
rendykml <rendikamaluddin2018@gmail.com>
2025-10-06 09:28:17 +0700

    push

--- part19/script.js
+++ part19/script.js
@@ -79,8 +79,21 @@ class CheckPoint {
       y,
     };
     this.width = proportionalSize(40);
-    this.height = proportionalSize(70)
+    this.height = proportionalSize(70);
+    this.claimed = false;
   };
+
+  draw() {
+    ctx.fillStyle = "#f1be32";
+    ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+  }
+  claim(){
+    this.width = 0;
+    this.height = 0;
+    this.position.y = Infinity;
+    this.claimed = true
+  }
+
 };
 
 
