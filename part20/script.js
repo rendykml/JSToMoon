932bfe98ff2d40c6a850fd391f888b62cb86b9d4
rendykml <rendikamaluddin2018@gmail.com>
2025-10-10 12:45:08 +0700

    push

--- part20/script.js
+++ part20/script.js
@@ -1,20 +1,30 @@
-const scoreInputs = document.querySelectorAll("#score-options input")
-const scoreSpans = document.querySelectorAll("#score-options span")
-const roundElement = document.getElementById("current-round")
-const rollsElement = document.getElementById("current-round-rolls")
-const totalScoreElement = document.getElementById("total-score")
-const scoreHistory = document.getElementById("score-history")
-const listOfAllDice = document.querySelectorAll(".die")
-const rulesBtn = document.getElementById("rules-btn")
-const rulesContainer = document.querySelector(".rules-container")
-const rollDiceBtn = document.getElementById("roll-dice-btn")
-const keepScoreBtn = document.getElementById("keep-score-btn")
+const listOfAllDice = document.querySelectorAll(".die");
+const scoreInputs = document.querySelectorAll("#score-options input");
+const scoreSpans = document.querySelectorAll("#score-options span");
+const roundElement = document.getElementById("current-round");
+const rollsElement = document.getElementById("current-round-rolls");
+const totalScoreElement = document.getElementById("total-score");
+const scoreHistory = document.getElementById("score-history");
+const rollDiceBtn = document.getElementById("roll-dice-btn");
+const keepScoreBtn = document.getElementById("keep-score-btn");
+const rulesContainer = document.querySelector(".rules-container");
+const rulesBtn = document.getElementById("rules-btn");
 
-let diceValuesArr = []
-let rolls = 0
-let score = 0
-let round = 1
+let diceValuesArr = [];
 let isModalShowing = false;
+let score = 0;
+let round = 1; 
+let rolls = 0; 
+  
 
+rulesBtn.addEventListener("click", () => {
+  isModalShowing = !isModalShowing;
 
-
+  if (isModalShowing) {
+    rulesBtn.textContent = "Hide rules";
+    rulesContainer.style.display = "block";
+  } else {
+    rulesBtn.textContent = "Show rules";
+    rulesContainer.style.display = "none";
+  }
+});
