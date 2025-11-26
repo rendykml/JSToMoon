0557ae197375c2c3b7e659cf627bc00839c65612
rendykml <rendikamaluddin2018@gmail.com>
2025-11-26 08:59:44 +0700

    push

--- part22/script.js
+++ part22/script.js
@@ -9,6 +9,7 @@ const fetchData = async () => {
   try {
     const res = await fetch(forumLatest);
     const data = await res.json();
+    showLatestPosts(data);
   } catch (err) {
     console.log(err);
   }
@@ -16,3 +17,6 @@ const fetchData = async () => {
 
 fetchData();
 
+const showLatestPosts = (data) => {
+  const {topic_list, users} = data
+};
