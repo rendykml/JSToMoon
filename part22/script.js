09523c1f2a78ee65f8c4e9e618963db670636a0c
rendykml <rendikamaluddin2018@gmail.com>
2025-11-28 14:14:26 +0700

    push

--- part22/script.js
+++ part22/script.js
@@ -6,10 +6,8 @@ const avatarUrl = "https://sea1.discourse-cdn.com/freecodecamp";
 const postsContainer = document.getElementById("posts-container");
 
 const allCategories = {
-  299 : {
-    category : "Career Advice",
-    className : "career"
-  }
+  299: { category: "Career Advice", className: "career" },
+  409 : {category: "Project Feedback", className : "feedback"}
 };
 
 const timeAgo = (time) => {
