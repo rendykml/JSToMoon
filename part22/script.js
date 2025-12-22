a3d77bd160c2f9089460e7d380d5ab5d9a22a7fc
rendykml <rendikamaluddin2018@gmail.com>
2025-12-22 20:05:04 +0700

    Initial commit

--- part22/script.js
+++ part22/script.js
@@ -82,7 +82,7 @@ fetchData();
 const showLatestPosts = (data) => {
   const { topic_list, users } = data;
   const { topics } = topic_list;
-
+  
   postsContainer.innerHTML = topics.map((item) => {
     const {
       id,
