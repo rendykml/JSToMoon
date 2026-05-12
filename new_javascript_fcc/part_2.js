38a07fe7b1eb32853224f765a9c3f4bd0dab223a
rendykml <rendikamaluddin2018@gmail.com>
2026-05-12 09:11:23 +0700

    push part1

--- new_javascript_fcc/part_2.js
+++ new_javascript_fcc/part_2.js
@@ -2,6 +2,10 @@ const regex = /freeCodeCamp/;
 const test = regex.test("e");
 console.log(test);
 
+const regex = /freeCodeCamp/;
+const match = "freeCodeCamp".match(regex);
+console.log(match);
+
 const regex = /freecodecamp/;
 const str = "freecodecamp is rly kewl";
 const replaced = str.replace(regex, "freeCodeCamp");
