6d6a37402a375665af3c653223f328814a0998fc
rendykml <rendikamaluddin2018@gmail.com>
2026-05-12 10:15:13 +0700

    push part1

--- new_javascript_fcc/part_3.js
+++ new_javascript_fcc/part_3.js
@@ -1,1 +1,19 @@
 const regex = /^\d\d\d\d$/;
+
+const regex = /^\d{4}$/;
+
+const regex = /^\d{4}$/;
+console.log(regex.test("123")); // false
+console.log(regex.test("1234")); // true
+console.log(regex.test("12345")); // false
+console.log(regex.test("123456")); // false
+console.log(regex.test("1234567")); // false
+
+const regex = /^\d{4,}$/;
+
+const regex = /^\d{4,}$/;
+console.log(regex.test("123")); // false
+console.log(regex.test("1234")); // true
+console.log(regex.test("12345")); // true
+console.log(regex.test("123456")); // true
+console.log(regex.test("1234567")); // true
