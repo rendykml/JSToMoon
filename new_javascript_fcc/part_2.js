d1249c2e02bace177cd58068bd20de34c684b649
rendykml <rendikamaluddin2018@gmail.com>
2026-05-12 09:24:43 +0700

    push part1

--- new_javascript_fcc/part_2.js
+++ new_javascript_fcc/part_2.js
@@ -11,7 +11,6 @@ const str = "freecodecamp is rly kewl";
 const replaced = str.replace(regex, "freeCodeCamp");
 console.log(replaced);
 
-
 //--------------------------------------//
 
 const regex = /freeCodeCamp/;
@@ -33,3 +32,33 @@ console.log(regex.test("FREECODECAMP")); // true
 console.log(regex.test("free")); // false
 console.log(regex.test("code")); // false
 console.log(regex.test("camp")); // false
+
+const regex = /freeCodeCamp/gi;
+
+console.log(regex.lastIndex); // 0
+console.log(regex.test("freeCodeCamp")); // true
+console.log(regex.lastIndex); // 12
+console.log(regex.test("freeCodeCamp is great")); // false
+console.log(regex.lastIndex); // 0
+console.log(regex.test("I love freeCodeCamp")); // true
+console.log(regex.lastIndex); // 19
+console.log(regex.test("freecodecamp")); // false
+console.log(regex.lastIndex); // 0
+console.log(regex.test("FREECODECAMP")); // true
+console.log(regex.lastIndex); // 12
+console.log(regex.test("free")); // false
+console.log(regex.lastIndex); // 0
+console.log(regex.test("code")); // false
+console.log(regex.lastIndex); // 0
+console.log(regex.test("camp")); // false
+
+const start = /^freecodecamp/i;
+const end = /freecodecamp$/i;
+console.log(start.test("freecodecamp")); // true
+console.log(end.test("freecodecamp")); // true
+console.log(start.test("freecodecamp is great")); // true
+console.log(end.test("freecodecamp is great")); // false
+console.log(start.test("i love freecodecamp")); // false
+console.log(end.test("i love freecodecamp")); // true
+console.log(start.test("have met freecodecamp's founder")); // false
+console.log(end.test("have met freecodecamp's founder")); // false
