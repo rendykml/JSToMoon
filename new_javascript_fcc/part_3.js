d64eb428e9041ccc1fe356a9efe58f2f190b0b87
rendykml <rendikamaluddin2018@gmail.com>
2026-05-12 10:26:12 +0700

    push part1

--- new_javascript_fcc/part_3.js
+++ new_javascript_fcc/part_3.js
@@ -17,3 +17,38 @@ console.log(regex.test("1234")); // true
 console.log(regex.test("12345")); // true
 console.log(regex.test("123456")); // true
 console.log(regex.test("1234567")); // true
+
+const regex = /^\d{4,6}$/;
+
+const regex = /^\d{4,6}$/;
+console.log(regex.test("123")); // false
+console.log(regex.test("1234")); // true
+console.log(regex.test("12345")); // true
+console.log(regex.test("123456")); // true
+console.log(regex.test("1234567")); // false
+
+const regex = /^[a-zA-Z]\d{4,6}$/;
+
+const regex = /^[a-zA-Z]?\d{4,6}$/;
+console.log(regex.test("123")); // false
+console.log(regex.test("a1234")); // true
+console.log(regex.test("12345")); // true
+console.log(regex.test("az12345")); // false
+console.log(regex.test("X123456")); // true
+console.log(regex.test("1234567")); // false
+
+const regex = /^[a-zA-Z]*\d{4,6}$/;
+console.log(regex.test("123")); // false
+console.log(regex.test("a1234")); // true
+console.log(regex.test("12345")); // true
+console.log(regex.test("az12345")); // true
+console.log(regex.test("X123456")); // true
+console.log(regex.test("1234567")); // false
+
+const regex = /^[a-zA-Z]+\d{4,6}$/;
+console.log(regex.test("123")); // false
+console.log(regex.test("a1234")); // true
+console.log(regex.test("12345")); // false
+console.log(regex.test("az12345")); // true
+console.log(regex.test("X123456")); // true
+console.log(regex.test("1234567")); // false
