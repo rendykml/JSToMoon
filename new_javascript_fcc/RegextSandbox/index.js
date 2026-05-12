1ef8b93bcead7c4565a66fc8bc7d8e4794be1326
rendykml <rendikamaluddin2018@gmail.com>
2026-05-12 16:01:52 +0700

    push part1

--- new_javascript_fcc/RegextSandbox/index.js
+++ new_javascript_fcc/RegextSandbox/index.js
@@ -2,4 +2,18 @@ let regexPattern = document.getElementById("pattern");
 let stringToTest = document.getElementById("test-string");
 let testButton = document.getElementById("test-btn");
 let testResult = document.getElementById("result")
+let caseInsensitiveFlag = document.getElementById("i")
+let globalFlag = document.getElementById("g")
+const getFlags = () => {
+  let flags = "";
 
+  if (caseInsensitiveFlag.checked) {
+    flags += "i";
+  }
+
+  if (globalFlag.checked) {
+    flags += "g";
+  }
+
+  return flags;
+};
