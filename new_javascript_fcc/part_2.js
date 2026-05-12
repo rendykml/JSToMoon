dbef171fcc50e35de816330376e3d8fa03674e87
rendykml <rendikamaluddin2018@gmail.com>
2026-05-12 09:22:14 +0700

    push part1

--- new_javascript_fcc/part_2.js
+++ new_javascript_fcc/part_2.js
@@ -10,3 +10,26 @@ const regex = /freecodecamp/;
 const str = "freecodecamp is rly kewl";
 const replaced = str.replace(regex, "freeCodeCamp");
 console.log(replaced);
+
+
+//--------------------------------------//
+
+const regex = /freeCodeCamp/;
+console.log(regex.test("freeCodeCamp")); // true
+console.log(regex.test("freeCodeCamp is great")); // true
+console.log(regex.test("I love freeCodeCamp")); // true
+console.log(regex.test("freecodecamp")); // false
+console.log(regex.test("FREECODECAMP")); // false
+console.log(regex.test("free")); // false
+console.log(regex.test("code")); // false
+console.log(regex.test("camp")); // false
+
+const regex = /freeCodeCamp/i; //This can also work for a string with a random mix of uppercase and lowercase letters:
+console.log(regex.test("freeCodeCamp")); // true
+console.log(regex.test("freeCodeCamp is great")); // true
+console.log(regex.test("I love freeCodeCamp")); // true
+console.log(regex.test("freecodecamp")); // true
+console.log(regex.test("FREECODECAMP")); // true
+console.log(regex.test("free")); // false
+console.log(regex.test("code")); // false
+console.log(regex.test("camp")); // false
