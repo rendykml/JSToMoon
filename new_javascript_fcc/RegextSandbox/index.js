08d1aeaf98d616b1078bbfa79b680aed30df4b50
rendykml <rendikamaluddin2018@gmail.com>
2026-05-12 11:00:59 +0700

    push part1

---
+++ new_javascript_fcc/RegextSandbox/index.js
@@ -0,0 +1,5 @@
+let regexPattern = document.getElementById("pattern");
+let stringToTest = document.getElementById("test-string");
+let testButton = document.getElementById("test-btn");
+let testResult = document.getElementById("result")
+
