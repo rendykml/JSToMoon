ef7f18242b578861f34e0d17d6e4b5dae48c4f54
rendykml <rendikamaluddin2018@gmail.com>
2026-05-12 09:58:52 +0700

    push part1

--- new_javascript_fcc/part_2.js
+++ new_javascript_fcc/part_2.js
@@ -130,3 +130,13 @@ console.log(Array.from(matched));
 //   input: 'freecodecamp is the best we love freecodecamp',
 //   groups: undefined
 // ]
+
+// --------------------------------------- //
+const regex = /free(?=code)/i;
+
+const regex = /free(?=code)/i;
+console.log(regex.test("freeCodeCamp")); // true
+console.log(regex.test("free code camp")); // false
+console.log(
+  regex.test("I need someone for free to write code for me")
+); // false
